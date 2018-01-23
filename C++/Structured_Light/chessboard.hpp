eef3b8289adf773e02f95cb22d1babaa26bd8661
mainero <mainerofrancisco@gmail.com>
2018-01-23 14:28:41 -0300

    Cambios importantes
    Chessboard completa, ordenamiento terminado, se decide pasar la union con los puntos reales del objeto para el momento de calibrar (otro parametro para la funcion de calibracion)
    Hasta este punto:
       - Features (deteccion descripcion y matching)
       - Chessboard (deteccion, filtrado de esquinas y ordenamiento)
       - Calibracion (plane based completa, componentes extrinsicas dependen de la chessboard, considerar cambiar)
       - Luz Estructurada (Codificacion y decodificacion completos)
    
    Falta:
       - Presicion (correcciones de presicion en deteccion de features, subpixel presicion)
       - Desprender componentes extrinsicas de las chessboard (featres y posiciones relativas)
       - Prueba definitiva para Luz Estructurada (poca calidad de fotos molesta, tomaarse el tiempo y probar bien)
       - Prueba de calibraicon completa para features.
       - Arreglar el repositorio y poner las cosas mas prolijas (es todo un asco)
       - Documentacion (TODA, URGENTE!)

--- C++/Structured_Light/chessboard.hpp
+++ C++/Structured_Light/chessboard.hpp
@@ -75,13 +75,60 @@ Mat rotate_mask(Mat mask, double angle)
 }
 
 ///
-//		4*1
-///FALTA! -> AJUSTAR EL THRESHOLD DINAMICO HASTA ENCONTRAR LA CANTIDAD DE PUNTOS DE LA CHESSBOARD O LOS MAS POSIBLES (POSIBLEMENTE DEDUCIR EL RESTO
-///		  -> RELACIONAR CON LOS PUNTOS DE REFERENCIA REALES	(PASADOS COMO PARAMETRO)
+///		  -> RELACIONAR CON LOS PUNTOS DE REFERENCIA REALES	(PASADOS COMO PARAMETRO) ->  RESUELTO: EN REALIDAD SE HACE AL CALIBRAR CON LA PLANILLA
 ///		  -> PROBAR USARLA PARA CALIBRAR 1 CAMARA RESPECTA A LA CHESSBOARD PRIMERO, LUEGO 2 CAMARAS RESPECTO A LA CHESSBOARD VISTA AL MISMO TIEMPO,
 ///				Y FINALMENTE CALIBRAR SOLO PARAMETROS INTERNOS Y DEDUCIR LA POSICION DE LA CAMARAS RELATIVAS ENTRE SI (REQUIERE UNION CON FEATURES MAS COMPLEJA)
+
+int dividir(vector<Point2f> &array, int start, int end) {
+	int left;
+	int right;
+	Point2f pivote, temp;
+	
+	pivote = array[start];
+	left = start;
+	right = end;
+	
+	// Mientras no se cruzen los índices
+	while (left < right) {
+		while (norm(array[right]) > norm(pivote)) {
+			right--;
+		}
+		
+		while ((left < right) && (norm(array[left]) <= norm(pivote))) {
+			left++;
+		}
+		
+		// Si todavía no se cruzan los indices seguimos intercambiando
+		if (left < right) {
+			temp = array[left];
+			array[left] = array[right];
+			array[right] = temp;
+		}
+	}
+	
+	// Los índices ya se han cruzado, ponemos el pivot en el lugar que le corresponde
+	temp = array[right];
+	array[right] = array[start];
+	array[start] = temp;
+	
+	// La nueva posición del pivot
+	return right;
+}
+
+void quicksort_points(vector<Point2f> &array, int start, int end)
+{
+	int pivote;
+	
+	if(start < end)
+	{
+		pivote = dividir(array,start,end);
+		
+		quicksort_points(array, start, pivote-1);
+		quicksort_points(array, pivote+1, end);
+	}
+}
+
 vector<Point2f> chessboard_features(Mat img,Size grid_features, Size size_nms, float thresh, Mat mask)
-//vector<Mat> chessboard_features(Mat img,Size grid_features, vector<Point2f> obj_position, Size size_nms, int thresh, Mat mask)
 {
 	Mat gris, feature, r1, r2;
 	Mat diff;
@@ -108,7 +155,6 @@ vector<Point2f> chessboard_features(Mat img,Size grid_features, Size size_nms, f
 	
 	///
 	angle=0.0f;
-//	Scalar media_global = mean(gris);
 	
 	///EXTRA AUX VARS, BORRAR LUEGO, SON SOLO PARA PROBAR COSAS
 	double minVal, maxVal;
@@ -174,19 +220,179 @@ vector<Point2f> chessboard_features(Mat img,Size grid_features, Size size_nms, f
 	else
 	{
 		features.clear();
+		return features;
 	}
 	
 	/// ASOCIO LAS FEATURES DESCUBIERTAS CON LOS PUNTOS DEL MUNDO REAL PASADOS COMO PARAMETROS.
 	// Las posiciones vienen: de abajo para arriba, de izquierda a derecha, 
 	//		en un solo vector alargado de tamanio de la grilla (grilla.x*grilla.y) 
+	vector<Point2f> ordenado(grid_features.width * grid_features.height);
+	vector<Point2f> linea;
+	
+	int indice;
+	float dist;
+	double cross, mag;
+	Point2f o, aux_p,  p1, p2;	
 	
 	
+	// Obtengo el origen
+	dist = gris.rows;
+	for(int i=0;i<features.size();i++) 
+	{
+		if(norm(features[i])<dist)
+		{
+			indice = i;
+			dist = norm(features[i]);
+		}
+	}
 	
-	return features;
+	o = features[indice];	// Punto mas cercano al origen de la fotografia
+	features[indice] = features[features.size()-1];		// Lo borro de features
+	features.pop_back();
+	
+	// Obtengo linea mas izquierda
+	indice = 0;
+	for(int i=1;i<features.size();i++) 
+	{
+		p1 = features[i] - o;
+		p2 = features[indice] - o;
+		cross = p1.cross(p2);
+		
+		if(cross<0)
+		{
+			indice = i;
+		}
+		
+	}
+	
+	aux_p = features[indice];
+	features[indice] = features[features.size()-1];		// Elimino de las Features
+	features.pop_back();
+		
+	// Saco puntos de la linea mas izquierda
+	linea.push_back(o);
+	linea.push_back(aux_p);
+	for(int i=0;i<features.size();i++) 
+	{
+		p1 = aux_p - o;
+		p2 = features[i] - o;
+		
+		cross = p1.cross(p2);	// Area paralelogramo
+		mag = norm(p1);			// Base paralelogramo
+		
+		if(abs(cross/mag)<10.0f)
+		{
+			
+			linea.push_back(features[i]);									// Agrego al arreglo para ordenar
+			
+			features[i] = features[features.size()-1];						// Elimino el punto de Features
+			features.pop_back();
+			
+			i--;															// Corrigo posicion del arreglo
+		}
+	}
+	
+	/// REVISAR EL LARGO SEA EL CORRECTO (grid_features.height)
+	
+	// Ordeno lo anterior
+	for(int i=0;i<linea.size();i++) 
+	{
+		linea[i] = linea[i] - o;	
+	}
+	
+	quicksort_points(linea,0,linea.size()-1);
+	
+	for(int i=0;i<linea.size();i++) 
+	{
+		ordenado[i*grid_features.width] = linea[i] + o;	
+	}
+
+	linea.clear();
+	//
+	for(int i=0;i<grid_features.height;i++) 
+	{
+		
+		// Obtengo linea mas derecha
+		indice = 0;
+		o = ordenado[i*grid_features.width];
+		for(int j=0;j<features.size();j++) 
+		{
+			p1 = features[j] - o;
+			p2 = features[indice] - o;
+			cross = p1.cross(p2);
+			
+			if(cross>0)
+			{
+				indice = j;
+			}
+			
+		}
+		
+		aux_p = features[indice];
+		features[indice] = features[features.size()-1];		// Elimino de las Features
+		features.pop_back();
+		
+		// Saco puntos de la linea mas derecha
+		linea.push_back(o);
+		linea.push_back(aux_p);
+		for(int j=0;j<features.size();j++) 
+		{
+			p1 = aux_p - o;
+			p2 = features[j] - o;
+			
+			cross = p1.cross(p2);	// Area paralelogramo
+			mag = norm(p1);			// Base paralelogramo
+			
+			if(abs(cross/mag)<10.0f)
+			{
+				
+				linea.push_back(features[j]);									// Agrego al arreglo para ordenar
+				
+				features[j] = features[features.size()-1];						// Elimino el punto de Features
+				features.pop_back();
+				
+				j--;															// Corrigo posicion del arreglo
+			}
+		}
+		
+		
+		// Ordeno lo anterior
+		for(int j=0;j<grid_features.width;j++)
+		{
+			linea[j] = linea[j] - o;	
+		}
+		
+		quicksort_points(linea,0,linea.size()-1);
+		
+		for(int j=0;j<grid_features.width;j++) 
+		{
+			ordenado[i*grid_features.width + j] = linea[j] + o;	
+		}
+		
+		linea.clear();
+	}
+	
+	
+	///
+//	Mat copy = img.clone();
+//	for(int i=0;i<ordenado.size();i++) 
+//	{
+//		circle(copy,ordenado[i],5,Scalar(0,255,0));
+//		show_mat(copy,IMAGEN,0);
+//		waitKey(0);
+//	}
+//	
+//	line(copy,o,izq,Scalar(0,255,0));
+//	line(copy,o,der,Scalar(0,255,0));
+	
+	
+//	circle(img,izq,5,Scalar(0,255,0));
+//	line(img,o,izq,Scalar(0,255,0));
+	
+	///
+	return ordenado;
 	
 }
 #endif
-
-
 
 /// REVISAR SIFT DENUEVO, HABIA UN NORM() AL REVES! POR ESO ESTABA DADO VUELTA.
